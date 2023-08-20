5b7bc6652e9ff61702aaf87499c4bc17168308c4
Abdulsalam <mr.smady@gmail.com>
2023-08-20 15:17:45 +0300

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -7,21 +7,27 @@ chmod +x gradlew
 
 
 cd ~/rms-backend/alarms-service || goto exit_error
+chmod +x ./*.sh
 ./build-image.sh
 ./run-service.sh
 cd ~/rms-backend/companies-service || goto exit_error
+chmod +x ./*.sh
 ./build-image.sh
 ./run-service.sh
 cd ~/rms-backend/graphql-service || goto exit_error
+chmod +x ./*.sh
 ./build-image.sh
 ./run-service.sh
 cd ~/rms-backend/manifest-service || goto exit_error
+chmod +x ./*.sh
 ./build-image.sh
 ./run-service.sh
 cd ~/rms-backend/trips-service || goto exit_error
+chmod +x ./*.sh
 ./build-image.sh
 ./run-service.sh
 cd ~/rms-backend/vehicles-service || goto exit_error
+chmod +x ./*.sh
 ./build-image.sh
 ./run-service.sh
 cd ~/rms-backend || goto exit_error
