a362db0ca794a117e815b8ac4d8b58d7b273106c
Abdulsalam <mr.smady@gmail.com>
2023-08-20 15:14:44 +0300

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 git clone git@github.com:mr-smady/rms-backend.git
-cd rms-backend || goto exit_error
+cd ~/rms-backend || goto exit_error
 chmod +x gradlew
+ ./gradlew clean build
 
 
-
-cd ~/services/alarms-service || goto exit_error
+cd ~/rms-backend/alarms-service || goto exit_error
 ./build-image.sh
 ./run-service.sh
-cd ~/services/companies-service || goto exit_error
+cd ~/rms-backend/companies-service || goto exit_error
 ./build-image.sh
 ./run-service.sh
-cd ~/services/graphql-service || goto exit_error
+cd ~/rms-backend/graphql-service || goto exit_error
 ./build-image.sh
 ./run-service.sh
-cd ~/services/manifest-service || goto exit_error
+cd ~/rms-backend/manifest-service || goto exit_error
 ./build-image.sh
 ./run-service.sh
-cd ~/services/trips-service || goto exit_error
+cd ~/rms-backend/trips-service || goto exit_error
 ./build-image.sh
 ./run-service.sh
-cd ~/services/vehicles-service || goto exit_error
+cd ~/rms-backend/vehicles-service || goto exit_error
 ./build-image.sh
 ./run-service.sh
-cd ~/services || goto exit_error
+cd ~/rms-backend || goto exit_error
 exit
 exit_error:
 echo 'ERROR'
